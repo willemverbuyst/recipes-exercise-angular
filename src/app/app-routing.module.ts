fa53cc5e830c63f22ae939865666e5f1f3ae521d
Willem Verbuyst <willemverbuyst@gmail.com>
2020-09-17 17:12:25 +0200

    Add child routes for recipe-items

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
+import { RecipeStartComponent } from './recipes/recipe-start/recipe-start.component';
 import { RecipesComponent } from './recipes/recipes.component';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 
@@ -8,6 +10,10 @@ const appRoutes: Routes = [
   {
     path: 'recipes',
     component: RecipesComponent,
+    children: [
+      { path: '', component: RecipeStartComponent },
+      { path: ':id', component: RecipeDetailComponent },
+    ],
   },
   {
     path: 'shopping-list',
