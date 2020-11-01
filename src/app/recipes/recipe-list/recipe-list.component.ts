c16080b66e217092ce7fdb90003adb21857b7861
Willem Verbuyst <willemverbuyst@gmail.com>
2020-11-01 08:35:22 +0100

    Implement submit form

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -18,6 +18,9 @@ export class RecipeListComponent implements OnInit {
   ) {}
 
   ngOnInit(): void {
+    this.recipeService.recipesChanged.subscribe((recipes: Recipe[]) => {
+      this.recipes = recipes;
+    });
     this.recipes = this.recipeService.getRecipes();
   }
 
