44d99d5493133a0d5af44c8e4e8c6db6fc41f870
Willem Verbuyst <willemverbuyst@gmail.com>
2020-11-03 14:21:14 +0100

    Set up login form

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,6 +6,7 @@ import { RecipeStartComponent } from './recipes/recipe-start/recipe-start.compon
 import { RecipesComponent } from './recipes/recipes.component';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { RecipesResolverService } from './recipes/recipes-resolver.service';
+import { AuthComponent } from './auth/auth.component';
 
 const appRoutes: Routes = [
   { path: '', redirectTo: '/recipes', pathMatch: 'full' },
@@ -31,6 +32,7 @@ const appRoutes: Routes = [
     path: 'shopping-list',
     component: ShoppingListComponent,
   },
+  { path: 'auth', component: AuthComponent },
 ];
 
 @NgModule({
